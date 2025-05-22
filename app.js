0252f0ac29877377b089110997e6b086062b8edd
shamil <shamilmohd418@gmail.com>
2025-05-22 19:36:19 +0530

    Added Sample Route

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('My Job Application');
+});
+
 
 
 
